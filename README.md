978da9832e037e971daad91aca95557f5e706f11
sebaF96 <sefernandez96@gmail.com>
2019-10-12 02:28:23 -0300

    agregada funcion load_dotenv para cargar automaticamente las variable de entorno

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>Inmobiliaria de alquileres</h1>
+<h3> Universidad de Mendoza</h3>
--- database.py
+++ database.py
@@ -2,9 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
 
+
+load_dotenv(override=True)
 
 Base = declarative_base()
-engine = create_engine('mysql+pymysql://'+os.environ.get('DB_CONECTION')+'@localhost/inmobiliaria')
+engine = create_engine('mysql+pymysql://'+os.getenv('DB_CONECTION')+'@localhost/inmobiliaria')
 # Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
