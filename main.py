5da007a65e92627159035da92e7577a75858bc4a
sebaF96 <sefernandez96@gmail.com>
2019-10-14 20:17:43 -0300

    agregada funcion de listar alquileres por DNI y listar clientes por DNI con sus respectivos alquileres y propiedades

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import curses
-from funciones import AgregarCliente, AgregarPropiedad, AgregarAlquiler
+from funciones import *
 from modelos import Cliente, Inmueble, Alquiler
-from database import Session
+from database import Session, Base
+from sqlalchemy import or_
 import time
 import os
 from datetime import datetime
 
 session = Session()
-opciones = ["Registrar cliente", "Añadir propiedad", "Registrar alquiler", "Ver alquileres", "Borrar alquiler", "Salir"]
+opciones = ["Registrar cliente", "Listar propiedades", "Añadir propiedad", "Registrar alquiler", "Ver alquileres",
+            "Ver clientes", "Salir"]
 
 
 def print_menu(stdscr, selected_row_idx):
@@ -15,8 +17,8 @@ def print_menu(stdscr, selected_row_idx):
     h, w = stdscr.getmaxyx()
 
     for idx, row in enumerate(opciones):
-        x = w//2 - len(row)//2
-        y = h//2 - len(opciones)//2 + idx
+        x = w // 2 - len(row) // 2
+        y = h // 2 - len(opciones) // 2 + idx
         if idx == selected_row_idx:
             stdscr.attron(curses.color_pair(1))
             stdscr.addstr(y, x, row)
@@ -28,6 +30,7 @@ def print_menu(stdscr, selected_row_idx):
 
 
 def main(stdscr):
+    global alquileres
     curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
@@ -47,20 +50,28 @@ def main(stdscr):
             current_row_idx = current_row_idx + 1
         elif key == curses.KEY_ENTER or key in [10, 13]:
             stdscr.refresh()
+            curses.endwin()
+            os.system('clear')
 
             if current_row_idx == 0:
-                curses.endwin()
-                os.system('clear')
                 cliente = AgregarCliente()
                 print("\nCliente añadido con exito!\n")
                 print(cliente)
                 session.add(cliente)
                 session.commit()
-                time.sleep(3)
 
             elif current_row_idx == 1:
-                curses.endwin()
-                os.system('clear')
+
+                casas_disponibles = session.query(Inmueble).filter(Inmueble.alquilado == 0).order_by(
+                    Inmueble.inmuebleId).all()
+
+                for casa in casas_disponibles:
+                    print(casa.inmuebleId, casa)
+
+                goback = str(input("\n\nPresione una tecla para volver al menu..."))
+
+            elif current_row_idx == 2:
+
                 ownerdni = str(input("Ingrese el dni del dueño de la propiedad: "))
                 cliente = session.query(Cliente).filter(Cliente.dni == ownerdni).one()
                 inmueble = AgregarPropiedad(cliente.clienteId)
@@ -70,9 +81,7 @@ def main(stdscr):
                 session.commit()
                 time.sleep(3)
 
-            elif current_row_idx == 2:
-                curses.endwin()
-                os.system('clear')
+            elif current_row_idx == 3:
                 dni_inquilino = str(input("Ingrese DNI del inquilino: "))
                 inquilino = session.query(Cliente).filter(Cliente.dni == dni_inquilino).one()
 
@@ -97,10 +106,19 @@ def main(stdscr):
 
                 session.commit()
 
-            elif current_row_idx == 3:
-                curses.endwin()
-                os.system('clear')
-                alquileres = session.query(Alquiler).order_by(Alquiler.fechainicio).all()
+            elif current_row_idx == 4:
+                print("1. DNI dueño\n2. DNI inquilino\n3. Todos")
+                choice = int(input())
+                if choice == 1:
+                    dni = int(input("Ingrese DNI de cliente "))
+                    alquileres = session.query(Alquiler).join(Inmueble).join(Cliente).filter(Cliente.dni == dni).all()
+
+                elif choice == 2:
+                    dni = int(input("Ingrese DNI del cliente "))
+                    alquileres = session.query(Alquiler).join(Cliente).filter(Cliente.dni == dni).all()
+
+                elif choice == 3:
+                    alquileres = session.query(Alquiler).order_by(Alquiler.fechainicio).all()
 
                 for alquiler in alquileres:
                     print("\n")
@@ -108,16 +126,19 @@ def main(stdscr):
 
                 goback = str(input("\n\nPresione una tecla para volver al menu..."))
 
-          #  elif current_row_idx == 4:
-
-
-
+            elif current_row_idx == 5:
+                dni = int(input("Ingrese el DNI del cliente "))
+                cliente = session.query(Cliente).filter(Cliente.dni == dni).one()
+                mostrar_cliente(cliente)
+                alquiler = session.query(Alquiler).filter(Alquiler.inquilino == cliente).one()
+                print("Alquilando: " + str(alquiler.inmueble))
+                goback = str(input("\n\nPresione una tecla para volver al menu..."))
 
             elif current_row_idx == len(opciones) - 1:
-                curses.endwin()
-                os.system('clear')
                 print("Hasta luego!")
-                time.sleep(2)
+                for x in range(7):
+                    time.sleep(0.2)
+                    print(".")
                 os.system('clear')
                 break
 
